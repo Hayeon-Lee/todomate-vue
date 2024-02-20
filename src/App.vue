d3ae4148ed9f15967e85b3548768a07e8ddd4830
이하연 <papepopepe@naver.com>
2024-02-20 17:48:09 +0900

    feat: ToDo 컴포넌트 생성

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,15 @@
 <script setup>
+import ToDoItem from "./components/ToDoItem.vue";
 </script>
 
 <template>
   <div id="app">
     <h1>To-Do List</h1>
+    <ul>
+      <li>
+        <to-do-item></to-do-item>
+      </li>
+    </ul>
   </div>
 </template>
 
