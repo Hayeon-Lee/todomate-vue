2f9be4e90e37b46d2879a77539c808054cdf2c33
hayeon-lee <papepopepe@naver.com>
2024-02-26 13:58:17 +0900

    featL localstorage에 있는 할 일을 불러와 화면에 나타내는 작업

--- src/components/ToDoList.vue
+++ src/components/ToDoList.vue
@@ -1,12 +1,28 @@
 <template>
   <div>
-    list
+    <ul>
+      <li v-for="todoItem in toDoItems" v-bind:key="todoItem">{{todoItem}}</li>
+    </ul>
   </div>
 </template>
 
 <script>
 export default {
+  data: function() {
+    return {
+      toDoItems: []
+    }
+  },
 
+  created: function() {
+    if (localStorage.length > 0) {
+      for (var i = 0; i< localStorage.length; i++) {
+        if (localStorage.key(i) !== 'loglevel:webpack-dev-server') {
+          this.toDoItems.push(localStorage.key(i));
+        }
+      }
+    }
+  } //앱이 생성되자마자 실행되는 hook
 }
 </script>
 
