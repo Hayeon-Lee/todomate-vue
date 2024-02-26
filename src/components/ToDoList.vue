4cc6430a8201f61c0e893b279a4dfa3d1c5e9ee6
hayeon-lee <papepopepe@naver.com>
2024-02-26 14:12:39 +0900

    feat: todolist 각 할 일 삭제 버튼 추가 및 스타일링

--- src/components/ToDoList.vue
+++ src/components/ToDoList.vue
@@ -1,7 +1,12 @@
 <template>
   <div>
     <ul>
-      <li v-for="todoItem in toDoItems" v-bind:key="todoItem">{{todoItem}}</li>
+      <li class="shadow" v-for="todoItem in toDoItems" v-bind:key="todoItem">
+        {{todoItem}}
+        <span class="removeBtn" v-on:click="removeTodo">
+          <i class="fa-solid fa-trash"></i>
+        </span>
+      </li>
     </ul>
   </div>
 </template>
@@ -14,7 +19,12 @@ export default {
     }
   },
 
-  created: function() {
+  methods: {
+    removeTodo: function() {
+    }
+  },
+  
+  created: function() { //앱이 생성되자마자 실행되는 hook
     if (localStorage.length > 0) {
       for (var i = 0; i< localStorage.length; i++) {
         if (localStorage.key(i) !== 'loglevel:webpack-dev-server') {
@@ -22,10 +32,48 @@ export default {
         }
       }
     }
-  } //앱이 생성되자마자 실행되는 hook
+  }, 
+
 }
 </script>
 
-<style>
+<style scoped>
+ul {
+  list-style-type: none;
+  padding-left: 0px;
+  margin-top: 0;
+  text-align: left;
+}
+
+li {
+  display: flex;
+  min-height: 50px;
+  height: 50px;
+  line-height: 50px;
+  margin: 0.5rem 0;
+  padding: 0 0.9rem;
+  background: white;
+  border-radius: 5px;
+}
+
+.removeBtn {
+  margin-left: auto;
+  color: #de4343;
+}
+
+.checkBtn {
+  line-height: 45px;
+  color: #62acde;
+  margin-right: 5px;
+}
+
+.checkBtnCompleted {
+  color: #b3adad;
+}
+
+.textCompleted {
+  text-decoration: line-through;
+  color: #b3adad;
+}
 
 </style>
