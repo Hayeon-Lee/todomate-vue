1eb8bd9b0420f5c178964647c3a6671954b167e9
이하연 <papepopepe@naver.com>
2024-02-26 15:56:16 +0900

    feat: todoItems를 props로 만들어 모든 곳에서 동일하게 사용하게 함

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <ToDoHeader></ToDoHeader>
   <ToDoInput></ToDoInput>
-  <ToDoList></ToDoList>
+  <ToDoList v-bind:propsdata="todoItems"></ToDoList> <!--v-bind:내려보낼 props 속성 이름="현재 위치의 컴포넌트 데이터 속성"-->
   <ToDoFooter></ToDoFooter>
 </template>
 
@@ -13,12 +13,28 @@ import ToDoFooter from './components/ToDoFooter.vue';
 
 
 export default {
+  data: function(){
+    return {
+      todoItems: [],
+    }
+  },  
+
   components: {
     'ToDoHeader': ToDoHeader,
     'ToDoInput': ToDoInput,
     'ToDoList': ToDoList,
     'ToDoFooter': ToDoFooter
   },
+
+  created: function() {
+    if (localStorage.length > 0) {
+      for (var i = 0; i<localStorage.length; i++){
+        if(localStorage.key(i) !== 'loglevel:webpack-dev-server') {
+          this.todoItems.push(JSON.parse(localStorage.getItem(localStorage.key(i))));
+        }
+      }
+    }
+  }
 }
 </script>
 
