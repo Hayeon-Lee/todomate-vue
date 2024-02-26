45cc49048f03b73bb5a6d8e659cb5f5416600bea
hayeon-lee <papepopepe@naver.com>
2024-02-26 10:16:06 +0900

    feat: ToDoHeader 구현

--- src/components/ToDoHeader.vue
+++ src/components/ToDoHeader.vue
@@ -1,7 +1,7 @@
 <template>
-    <div>
-        header
-    </div>
+    <header>
+        <h1>TODO it!</h1>
+    </header>
 </template>
 
 <script>
@@ -10,6 +10,10 @@ export default {
 }
 </script>
 
-<style>
-
+<style scoped> /*scoped 를 사용하면, 이 파일 안에서만 스타일링이 적용된다*/
+h1 {
+    color: #2F3B52;
+    font-weight: 900;
+    margin: 2.5rem 0 1.5rem; /*rem은 폰트의 크기에 따라 배율이 정해진다*/
+}
 </style>
